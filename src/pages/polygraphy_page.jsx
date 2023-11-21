124fcb43f8cc46a3a59ca42075f3bcc4ae077b43
Clourincess <96530726+Clourincess@users.noreply.github.com>
2023-11-21 14:02:49 +0300

    release v1

--- src/pages/polygraphy_page.jsx
+++ src/pages/polygraphy_page.jsx
@@ -1,11 +1,33 @@
 import { VStack, Stack, HStack } from "@chakra-ui/react";
 
 import SideMenu from "../components/side_menu";
+import Header from "../components/header/header";
+import Footer from "../components/footer";
 
 const PolygraphyPage = () => {
   return (
-    <Stack direction={"row"} minH='100vh'>
+    <Stack 
+    direction={"row"}
+    height="auto"
+    spacing="0"
+    backgroundColor="menu_white"
+    width="100%"
+    >
       <SideMenu />
+      <VStack
+        marginLeft={[200, 200, 200, 210, 220]}
+        backgroundColor="menu_white"
+      >
+        <Header />
+        <VStack
+          padding={25}
+          alignItems="flex-start"
+          overflow="scroll"
+          spacing="40px"
+        >
+        </VStack>
+        <Footer />
+      </VStack>
     </Stack>
   );
 };
